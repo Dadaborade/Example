cd84d33714b842fb6fababb2d8b40ecdcdc18936
Dadaborade <dadaborade41@gmail.com>
2023-06-14 21:01:28 +0530

    first commit

---
+++ src/main/java/com/cjc/sendEmail/webapp/main/SprinBootPushRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.cjc.sendEmail.webapp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootPushRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootPushRepositoryApplication.class, args);
+	}
+
+}
